fb89f43658e5e67570fe6d3c0d60af82c73af72a
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2016-10-10 00:33:08 +0900

    correct indentation of PlainTextFormat

--- lib/dumb_down_viewer/tree_view_builder.rb
+++ lib/dumb_down_viewer/tree_view_builder.rb
@@ -8,60 +8,60 @@ module DumbDownViewer
     class PlainTextFormat
       attr_accessor :line
 
-    LINE_PATTERN = {
-      spacer: '     ',
-      h_line: '── ',
-      v_line: '│   ',
-      branch: '├─ ',
+      LINE_PATTERN = {
+        spacer: '     ',
+        h_line: '── ',
+        v_line: '│   ',
+        branch: '├─ ',
         corner: '└─ ' }
 
       def initialize(line_pattern=LINE_PATTERN)
         @line = line_pattern
       end
 
-    def format_table(tree_table)
-      t = tree_table.transpose
-      t.each_cons(2) do |fr, sr|
-        fr.each_with_index do |f, i|
-          next unless f.kind_of? Node
-          draw_lines(fr, sr, f, i)
+      def format_table(tree_table)
+        t = tree_table.transpose
+        t.each_cons(2) do |fr, sr|
+          fr.each_with_index do |f, i|
+            next unless f.kind_of? Node
+            draw_lines(fr, sr, f, i)
+          end
+        end
+
+        update_root_directory_name(tree_table[0][0], t)
+        fill_spaces(t.transpose).map {|r| r.join }.join($/) + $/
+      end
+
+      def draw_lines(fr, sr, f_node, i)
+        sub_count = f_node.sub_nodes.size
+        j = i
+        while sub_count > 0
+          j += 1
+          s_node = sr[j]
+          if s_node
+            fr[j] = sub_count == 1 ? @line[:corner] : @line[:branch]
+            sub_count -= 1
+          else
+            fr[j] = @line[:v_line]
+          end
+        end
+        fr[i] = f_node.kind_of?(DirNode) ? "[#{f_node.name}]" : f_node.name
+      end
+
+      def fill_spaces(table)
+        table.map do |row|
+          (row.size - 1).downto(0) do |i|
+            row[i] = @line[:spacer] if row[i + 1] and row[i].nil?
+          end
+          row
         end
       end
 
-      update_root_directory_name(tree_table[0][0], t)
-      fill_spaces(t.transpose).map {|r| r.join }.join($/) + $/
-    end
-
-    def draw_lines(fr, sr, f_node, i)
-      sub_count = f_node.sub_nodes.size
-      j = i
-      while sub_count > 0
-        j += 1
-        s_node = sr[j]
-        if s_node
-          fr[j] = sub_count == 1 ? @line[:corner] : @line[:branch]
-          sub_count -= 1
-        else
-          fr[j] = @line[:v_line]
+      def update_root_directory_name(root, table)
+        if root.directory and not root.directory.empty?
+          table[0][0] = "[#{File.join(root.directory, root.name)}]"
         end
       end
-      fr[i] = f_node.kind_of?(DirNode) ? "[#{f_node.name}]" : f_node.name
-    end
-
-    def fill_spaces(table)
-      table.map do |row|
-        (row.size - 1).downto(0) do |i|
-          row[i] = @line[:spacer] if row[i + 1] and row[i].nil?
-        end
-        row
-      end
-    end
-
-    def update_root_directory_name(root, table)
-      if root.directory and not root.directory.empty?
-        table[0][0] = "[#{File.join(root.directory, root.name)}]"
-      end
-    end
     end
 
     def setup(tree)
