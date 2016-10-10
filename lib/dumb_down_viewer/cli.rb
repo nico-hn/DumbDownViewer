374bae47d8de2e6fc7110565f70f9ab0411e1ac4
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2016-10-11 03:13:50 +0900

    add module Cli

---
+++ lib/dumb_down_viewer/cli.rb
@@ -0,0 +1,48 @@
+require 'dumb_down_viewer'
+require 'dumb_down_viewer/tree_view_builder'
+require 'optparse_plus'
+
+module DumbDownViewer
+  module Cli
+    FORMATTER = {
+      default: DumbDownViewer::TreeViewBuilder::PlainTextFormat,
+      csv: DumbDownViewer::TreeViewBuilder::CSVFormat,
+      tsv: DumbDownViewer::TreeViewBuilder::CSVFormat,
+      tree_csv: DumbDownViewer::TreeViewBuilder::TreeCSVFormat
+    }
+
+    OPTIONS = <<YAML
+banner: "USAGE: #{File.basename($0)} [OPTION]... DIRECTORY"
+style:
+  short: "-s [style_name]"
+  long: "--style [=style_name]"
+  description: "Choose the style of output other than default from ascii_art, list, zenkaku"
+format:
+  short: "-f [format_name]"
+  long: "--format [=format_name]"
+  description: Choose the output format other than default from csv, tsv, tree_csv
+YAML
+
+    def self.parse_command_line_options
+      options = { style: :default, format: :default }
+      OptionParser.new_with_yaml(OPTIONS) do |opt|
+        opt.inherit_ruby_options("E") # -E --encoding
+
+        opt.on(:style) {|style| options[:style] = style.to_sym }
+        opt.on(:format) {|format| options[:format] = format.to_sym }
+        opt.parse!
+      end
+      options
+    end
+
+    def self.execute
+      options = parse_command_line_options
+      col_sep = options[:format] == :csv ? ',' : "\t"
+      tree = DumbDownViewer.build_node_tree(ARGV[0])
+      style = options[:style]
+      builder = TreeViewBuilder.create(tree)
+      formatter = FORMATTER[options[:format]].new(style, col_sep)
+      print formatter.format_table(builder.tree_table)
+    end
+  end
+end
