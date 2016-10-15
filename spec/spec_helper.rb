8226061c528fdb33b74567d75f4a013dfe413c94
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2016-10-16 07:01:45 +0900

    add Helpers#set_argv

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,13 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+require 'shellwords'
 require "dumb_down_viewer"
+
+module Helpers
+  def set_argv(command_line_str)
+    ARGV.replace Shellwords.split(command_line_str)
+  end
+end
+
+RSpec.configure do |c|
+  c.include Helpers
+end
