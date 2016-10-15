962871bd4877f59314fbb9f7fecaa4ffc8b45cba
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2016-10-16 07:32:57 +0900

    add tests for command line option --format csv/tsv

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -59,5 +59,61 @@ RESULT
         DumbDownViewer::Cli.execute
       end
     end
+
+    describe '--format' do
+      it '--format csv' do
+        expected_result = <<RESULT
+spec/data,,,
+,README,,
+,index.html,,
+,aves,,
+,,index.html,
+,,can_fly,
+,,,sparrow.txt
+,,cannot_fly,
+,,,ostrich.jpg
+,,,ostrich.txt
+,,,penguin.jpg
+,,,penguin.txt
+,mammalia,,
+,,index.html,
+,,can_fly,
+,,,bat.txt
+,,cannot_fly,
+,,,elephant.txt
+RESULT
+
+        allow(STDOUT).to receive(:print).with(expected_result)
+        set_argv('--format csv spec/data')
+        DumbDownViewer::Cli.execute
+      end
+
+      it '--format tsv' do
+        expected_result = <<RESULT
+spec/data			
+	README		
+	index.html		
+	aves		
+		index.html	
+		can_fly	
+			sparrow.txt
+		cannot_fly	
+			ostrich.jpg
+			ostrich.txt
+			penguin.jpg
+			penguin.txt
+	mammalia		
+		index.html	
+		can_fly	
+			bat.txt
+		cannot_fly	
+			elephant.txt
+RESULT
+
+        allow(STDOUT).to receive(:print).with(expected_result)
+        set_argv('--format tsv spec/data')
+        DumbDownViewer::Cli.execute
+      end
+    end
   end
 end
