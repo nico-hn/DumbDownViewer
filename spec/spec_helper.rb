4e3062d97396e36ddc732c505ce30ed21b4fc45b
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2017-04-27 23:41:15 +0900

    add Helpers.prepare_test_data in spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,26 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require 'shellwords'
+require 'fileutils'
 require "dumb_down_viewer"
 
 module Helpers
   def set_argv(command_line_str)
     ARGV.replace Shellwords.split(command_line_str)
   end
+
+  def self.prepare_test_data
+    large_file = "#{__dir__}/data/mammalia/cannot_fly/elephant.txt"
+    unless File.exist?(large_file)
+      FileUtils.mkdir_p(File.dirname(large_file))
+      open(large_file, 'wb') do |file|
+        file.print '0' * 15_555_555
+      end
+    end
+  end
+
+  private_class_method :prepare_test_data
+
+  prepare_test_data
 end
 
 RSpec.configure do |c|
